71b55c5faf1cc4f983b5b7805f71c3b804b4b575
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-07 23:35:13 +0900

    init: backend init

---
+++ backend/personal-folio/src/main/java/jam/personal_folio/PersonalFolioApplication.java
@@ -0,0 +1,13 @@
+package jam.personal_folio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFolioApplication.class, args);
+	}
+
+}
