2a4a0a1d4ed4a48c863890fbfabc1542bacd16a4
JaeiKim <dobbykim0320@gmail.com>
2025-05-28 12:48:38 +0900

    init: next 필수 설정 수정

--- Frontend/personal-folio/next.config.ts
+++ Frontend/personal-folio/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // next/image 쓸 경우 필요
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
