360f6c4ceb0983693b2f47fce1c9b7f56d50a892
Jae2 <dobbykim0320@google.com>
2025-06-05 17:44:16 +0900

    design: design for register Page

--- Frontend/personal-folio/src/app/users/register/page.tsx
+++ Frontend/personal-folio/src/app/users/register/page.tsx
@@ -1,5 +1,27 @@
+import Input from "@/components/input/Input";
+
 const registerPage = () => {
-  return <div>조졌다</div>;
+  return (
+    <div className="h-200 w-150 flex flex-col items-center justify-center font-geist gap-3">
+      {/* Title */}
+      <div className="title-box">
+        <div>회원가입</div>
+      </div>
+      {/* Input */}
+      <div className="input-box">
+        <Input label="Email" placeholder="Id" />
+      </div>
+      <div className="input-box">
+        <Input label="Password" placeholder="Password" />
+      </div>
+
+      {/* Login Btn */}
+      <div className="btn-box">회원가입</div>
+
+      {/* br */}
+      {/* social login */}
+    </div>
+  );
 };
 
 export default registerPage;
